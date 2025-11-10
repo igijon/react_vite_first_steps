42af28313ea7145d5ad2d275294550f36925486c
Inma Gijón <igijoninf@gmail.com>
2025-11-10 13:28:19 +0100

    Otro componente

---
+++ src/MyAwesomeApp.tsx
@@ -0,0 +1,8 @@
+export function MyAwesomeApp() {
+    return(
+        <>
+            <h1>Inma</h1>
+            <h3>Gijón</h3>
+        </>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { FirstStepsApp } from './FistStepsApp'
+// import { FirstStepsApp } from './FistStepsApp'
+import { MyAwesomeApp } from './MyAwesomeApp'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FirstStepsApp/>
+    {/* <FirstStepsApp/> */}
+    <MyAwesomeApp />
+    <MyAwesomeApp />
+    <MyAwesomeApp />
   </StrictMode>,
 )
