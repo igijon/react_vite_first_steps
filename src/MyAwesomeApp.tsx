21d95599ae342ed2b0d444f7058d43062ab42b0a
Inma Gijón <igijoninf@gmail.com>
2025-11-10 19:22:05 +0100

    Impresión de variables

--- src/MyAwesomeApp.tsx
+++ src/MyAwesomeApp.tsx
@@ -1,8 +1,27 @@
-export function MyAwesomeApp() {
+// Las variables si no se modifican pueden estar fuera del componente y es más eficiente porque el código dentro del 
+// componente se ejecuta constantemente.
+const firstName = 'Inma';
+const lastName = 'Gijón';
+
+const favoriteGames = ['Elden Ring', 'Smash', 'Metal Gear'];
+const isActive = true;
+
+const address = {
+    zipCode: 'ABC-123',
+    country: 'Canadá'
+};
+export const MyAwesomeApp = () => {
+
     return(
         <>
-            <h1>Inma</h1>
-            <h3>Gijón</h3>
+            <h1>{firstName}</h1>
+            <h3>{lastName}</h3>
+            <p>{favoriteGames.join(', ')}</p>
+            <p>{2+2}</p>
+            {/* Un boolean no tiene representación por sí mismo en React */}
+            <h1>{isActive ? 'Activo': 'No activo'}</h1>
+            {/* Los objetos no son un tipo permitido en React como hijo.  Con los arrays no pasa*/}
+            <h1>{JSON.stringify(address)}</h1>
         </>
-    )
-}
+    );
+};
