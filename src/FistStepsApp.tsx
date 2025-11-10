8ac81002102811b4af333a097dee6480467ff2d6
Inma Gijón <igijoninf@gmail.com>
2025-11-10 19:47:57 +0100

    Propiedades del componente - Props

--- src/FistStepsApp.tsx
+++ src/FistStepsApp.tsx
@@ -4,7 +4,9 @@ export function FirstStepsApp() {
     return (
         <>
             <h1>Carro de la compra</h1>
-            <ItemCounter/>
+            <ItemCounter name="Nintendo Switch 2"  quantity={10}/>
+            <ItemCounter name="Pro Controller" quantity={3}/>
+            <ItemCounter name="Super Smash" quantity={2}/>
         </>
     )
 }
