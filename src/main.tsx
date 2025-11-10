e3750dda54146f2b15e42adaf1b70a5404aa2721
Inma Gijón <igijoninf@gmail.com>
2025-11-10 13:11:10 +0100

    Ejemplos de JSX

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <h1>Hola mundo</h1>
     <p>Esto es un párrafo</p>
+    <button>Click me</button>
+    <div>
+      <h2>Hola dentro de un div</h2>
+    </div>
   </StrictMode>,
 )
