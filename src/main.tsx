d39c145610d089884af1b8f218a5a1625ea6a79a
Inma Gijón <igijoninf@gmail.com>
2025-11-10 13:15:44 +0100

    Primer componente en React

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { FirstStepsApp } from './FistStepsApp'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hola mundo</h1>
-    <p>Esto es un párrafo</p>
-    <button>Click me</button>
-    <div>
-      <h2>Hola dentro de un div</h2>
-    </div>
+    <FirstStepsApp/>
   </StrictMode>,
 )
