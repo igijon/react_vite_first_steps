af0d25886dfdebeebee0839778a7bd140297c62d
Inma Gijón <igijoninf@gmail.com>
2025-11-15 16:32:03 +0100

    Agrupar pruebas similares

--- src/helpers/math.helper.test.ts
+++ src/helpers/math.helper.test.ts
@@ -1,13 +1,66 @@
 
-import {expect, test} from 'vitest'
-import { add } from './math.helper';
+import {describe, expect, test} from 'vitest'
+import { add, multiply, subtract } from './math.helper';
 
-test('should add two positives numbers', ()=>{
-    // ! 1. Arrange
-    const a = 1
-    const b = 2
-    // ! 2. Act
-    const result = add(a,b);
-    // ! 3. Assert
-    expect(result).toBe(a + b);
+describe('add', () => {
+    test('should add two positives numbers', ()=>{
+        // ! 1. Arrange
+        const a = 1
+        const b = 2
+        // ! 2. Act
+        const result = add(a,b);
+        // ! 3. Assert
+        expect(result).toBe(a + b);
+    })
+
+    test('should add two negative numbers', ()=>{
+        // ! 1. Arrange
+        const a = -1
+        const b = -2
+        // ! 2. Act
+        const result = add(a,b);
+        // ! 3. Assert
+        expect(result).toBe(a + b);
+    })
+})
+
+describe('subtract', () => {
+    test('should subtract two positives numbers', ()=>{
+        // ! 1. Arrange
+        const a = 1
+        const b = 2
+        // ! 2. Act
+        const result = subtract(a,b);
+        // ! 3. Assert
+        expect(result).toBe(a - b);
+    })
+    test('should subtract two negative numbers', ()=>{
+        // ! 1. Arrange
+        const a = -3
+        const b = -2
+        // ! 2. Act
+        const result = subtract(a,b);
+        // ! 3. Assert
+        expect(result).toBe(a - b);
+    })
+})
+describe('multiply', () => {
+    test('should multiply two positives numbers', ()=>{
+        // ! 1. Arrange
+        const a = 1
+        const b = 2
+        // ! 2. Act
+        const result = multiply(a,b);
+        // ! 3. Assert
+        expect(result).toBe(a * b);
+    })
+    test('should multiply two negative numbers', ()=>{
+        // ! 1. Arrange
+        const a = -3
+        const b = -2
+        // ! 2. Act
+        const result = multiply(a,b);
+        // ! 3. Assert
+        expect(result).toBe(a * b);
+    })
 })
